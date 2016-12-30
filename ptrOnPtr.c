f89edafa2ccf7436229010b8582146f3d35398d2
joker <dunwenqiang@gmail.com>
2016-12-30 19:53:36 +0800

    add all file

---
+++ ptrOnPtr.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    int* p;
+    int** p2p = &p;
+    p = &i;
+    int j = 0;
+
+    // int* p = ;
+    // int** p2p, j = 0;
+
+    printf("i = %d\np = %d\np2p = %d\n**p2p = %d\nj = %d\n", i, \
+        *p, **p2p, **p2p, j);
+
+}
